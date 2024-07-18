a0b056176b9c05f1dd0700806b979b3454cd96ce
mason <conficker77@gmail.com>
2024-07-18 09:04:43 +0900

    백준 단계별 문제풀이 ~6-1

---
+++ src/백준/심화1/BOJ_25083.java
@@ -0,0 +1,14 @@
+package 백준.심화1;
+
+public class BOJ_25083 {
+    public static void main(String[] args) {
+        String str = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.println(str);
+    }
+}
